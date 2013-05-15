8ea5df3b8fe569cd080ecc665b1a4ab3a4e48268
David Vrensk <david@vrensk.com>
2013-05-15 19:49:38 +0200

    2 tests, 2 failures, 0 errors

--- python/test_train_reservation.py
+++ python/test_train_reservation.py
@@ -5,7 +5,7 @@ from ticket_office import *
 
 class TestTicketOffice(unittest.TestCase):
 
-    def test_reserve_seats(self):
+    def test0_reserve_seats(self):
         office = TicketOffice(train_data_service = "http://localhost:8081", 
                               booking_reference_service = "http://localhost:8082")
         request = ReservationRequest(train_id="express_2000", seat_count=4)
@@ -15,6 +15,23 @@ class TestTicketOffice(unittest.TestCase):
         self.assertEqual(4, len(reservation.seats))
         self.assertEqual("A", reservation.seats[0].coach)
         self.assertEqual("75bcd15", reservation.booking_reference)
+
+    def test_reserve_seats_internal(self):
+        train = { "seats": [{ "coach" : "A", "seat_number" : "1" },
+                            { "coach" : "A", "seat_number" : "2" }]}
+        data_service = mock()
+        when(data_service).get("express_2000").thenReturn(train)
+        ref_service = mock() ; when(ref_service).get().thenReturn('75bcd15')
+        handler = TicketHandler(train_data_service = data_service,
+                               booking_reference_service = ref_service)
+        request = ReservationRequest(train_id="express_2000", seat_count=2)
+        reservation = handler.make_reservation(request)
+        
+        self.assertEqual(2, len(reservation.seats))
+        self.assertEqual("A", reservation.seats[0].coach)
+        self.assertEqual("75bcd15", reservation.booking_reference)
+
+
 
 if __name__ == '__main__':
     unittest.main()
--- python/ticket_office.py
+++ python/ticket_office.py
@@ -4,9 +4,15 @@ from reservations import *
 class TicketOffice(object):
 
     def __init__(self, train_data_service, booking_reference_service):
-        pass
+        self.ticket_handler = TicketHandler(DataService(train_data_service), BookingReferenceService(booking_reference_service))
 
     def make_reservation(self, reservation_request):
-        # TODO: write this code!
-        pass
+        return self.ticket_handler.make_reservation(reservation_request)
 
+class TicketHandler(object):
+    def __init__(self, train_data_service, booking_reference_service):
+        self.train_data_service = train_data_service
+        self.booking_reference_service = booking_reference_service
+
+    def make_reservation(self, reservation_request):
+        return None
